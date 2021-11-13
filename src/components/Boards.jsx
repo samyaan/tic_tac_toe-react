80be24b00130faec3e99b1d34d23c65bc17ae2d8
= <=>
2021-11-14 00:57:04 +0530

    added state function and events

--- src/components/Boards.jsx
+++ src/components/Boards.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Square from './Square';
 
 
 const Boards = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [isXNext, setXNext] = useState(false);
+
+  const HandleSquare = (position) =>{
+   if(board[position]){
+     return;
+   }
+    setBoard(prev=>{
+      return prev.map((square, pos)=>{
+        if(pos==position){
+          return isXNext ? 'X' : 'O';
+        }
+        
+        return square;
+      });
+    });
+      setXNext((prev)=>!prev);
+
+  };
+  
+  const RenderSquare = (position) =>{
+    return <Square value={board[position]} onClick={()=>HandleSquare(position)} />
+    
+  };
   return (
-    <div>
-      <div>
-        <Square value={0} />
-        <Square  value={1} />
-        <Square value={2} />
+    <div className="board">
+      <div className="board-row">
+        {RenderSquare(0)}
+        {RenderSquare(1)}
+        {RenderSquare(2)}
+      </div> 
+      <div className="board-row">
+        {RenderSquare(3)}
+        {RenderSquare(4)}
+        {RenderSquare(5)}
       </div>
-      <div>
-      <Square value={3} />
-      <Square value={4} />
-      <Square value={5} />
-      </div>
-      <div>
-      <Square value={6} />
-      <Square value={7} />
-      <Square value={8} />
+      <div className="board-row">
+        {RenderSquare(6)}
+        {RenderSquare(7)}
+        {RenderSquare(8)}
       </div>
     </div>
   );
